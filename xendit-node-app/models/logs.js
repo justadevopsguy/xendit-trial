b872fd0e12222b44c98a679f89b0d1ebfd86635b
AJ Quiambao <ajquiambao011@gmail.com>
2021-01-29 07:53:03 +0800

    Added Initial Files

--- xendit-node-app/models/logs.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const monthSchema = mongoose.Schema({
-  month: Number,
-  year: Number
-}, { collection: 'months' });
-const Month = mongoose.model('Month', monthSchema);
-
-module.exports = Month;
